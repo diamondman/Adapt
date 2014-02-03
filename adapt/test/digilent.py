180ea92bdb1f6f2a70560968ab90790a688ebadd
Michael E. Karpeles <michael.karpeles@gmail.com>
2014-02-03 15:10:57 -0800

    added travis ci + placeholder test case

---
+++ adapt/test/digilent.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+"""
+    test.digilent
+    ~~~~~~~~~~~~~
+
+    Test cases for digilent driver for Linux USB JTAG controller
+
+    :copyright: (c) 2014 by Jessy Diamond Exum
+    :license: Pending, see LICENSE for more details.
+"""
+
+import unittest
+
+class TestDigilent(unittest.TestCase):
+
+    def setUp(self):
+        """Emulate device connection"""
+        pass
